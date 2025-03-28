04e1802b123429ae7273ffb0fc73c9d040352db3
Red_Phoenix05 <anson.fry@gmail.com>
2025-03-28 16:38:08 -0400

    Update Player Controller
    switched movement from fixed update to update
    improved code readability

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -45,69 +45,72 @@ public class PlayerMovementController : MonoBehaviour
         }
     }
 
-    private void FixedUpdate() // Handles movement control
-    {
-        if (valid)
-        {
-            // Get input vector
-            Vector2 inputDirection = moveAction.action.ReadValue<Vector2>();
-
-            // Create movement vector from 2d
-            Vector3 moveDirection = new(inputDirection.x, 0f, inputDirection.y);
-
-            // Calculate speed
-            float speed = baseSpeed;
-            if (sneakAction.action.IsPressed()) speed *= sneakModifier;
-            else if (sprintAction.action.IsPressed()) speed *= sprintModifier;
-
-            // Calculate horizontal movement
-            Vector3 horizontalFactor = speed * Time.fixedDeltaTime * moveDirection;
-
-            // Apply gravity
-            Vector3 gravityFactor = Vector3.zero;
-            if (!controller.isGrounded)
-            {
-                gravityFactor = -gravity * Time.fixedDeltaTime * Vector3.down;
-            }
-
-            // Move character
-            controller.Move(horizontalFactor + gravityFactor);
-        }
-    }
-
     private void Update() // Handles other input
     {
         if (valid)
         {
-            // Check for interaction
-            if (interactables.Count > 0)
-            {
-                float minDistSquared = Vector3.SqrMagnitude(interactionCaster.transform.position - interactables[0].transform.position);
-                Interactable closest = interactables[0];
-
-                // Get closest interactable
-                for (int i = 1; i < interactables.Count; i++)
-                {
-                    float distSquared = Vector3.SqrMagnitude(interactionCaster.transform.position - interactables[i].transform.position);
-                    if (distSquared < minDistSquared)
-                    {
-                        minDistSquared = distSquared;
-                        closest = interactables[i];
-                    }
-                }
-
-                // Set closest as main
-                closest.SetInteraction(true);
-                mainInteractable = closest;
-            }
-            else mainInteractable = null;
-
-            // Interaction
-            if (mainInteractable && interactAction.action.WasPressedThisFrame())
-            {
-                mainInteractable.Interact();
-            }
+            UpdateMovement();
+            UpdateInteraction();
         }
+    }
+
+    private void UpdateInteraction()
+    {
+        // Check for interaction
+        if (interactables.Count > 0)
+        {
+            float minDistSquared = Vector3.SqrMagnitude(interactionCaster.transform.position - interactables[0].transform.position);
+            Interactable closest = interactables[0];
+
+            // Get closest interactable
+            for (int i = 1; i < interactables.Count; i++)
+            {
+                float distSquared = Vector3.SqrMagnitude(interactionCaster.transform.position - interactables[i].transform.position);
+                if (distSquared < minDistSquared)
+                {
+                    minDistSquared = distSquared;
+                    closest = interactables[i];
+                }
+            }
+
+            // Set closest as main
+            closest.SetInteraction(true);
+            mainInteractable = closest;
+        }
+        else mainInteractable = null;
+
+        // Interaction
+        if (mainInteractable && interactAction.action.WasPressedThisFrame())
+        {
+            mainInteractable.Interact();
+        }
+    }
+
+    private void UpdateMovement()
+    {
+        // Get input vector
+        Vector2 inputDirection = moveAction.action.ReadValue<Vector2>();
+
+        // Create movement vector from 2d
+        Vector3 moveDirection = new(inputDirection.x, 0f, inputDirection.y);
+
+        // Calculate speed
+        float speed = baseSpeed;
+        if (sneakAction.action.IsPressed()) speed *= sneakModifier;
+        else if (sprintAction.action.IsPressed()) speed *= sprintModifier;
+
+        // Calculate horizontal movement
+        Vector3 horizontalFactor = speed * Time.deltaTime * moveDirection;
+
+        // Apply gravity
+        Vector3 gravityFactor = Vector3.zero;
+        if (!controller.isGrounded)
+        {
+            gravityFactor = -gravity * Time.deltaTime * Vector3.down;
+        }
+
+        // Move character
+        controller.Move(horizontalFactor + gravityFactor);
     }
 
     private void Cast_TriggerEnter(Collider collider)
