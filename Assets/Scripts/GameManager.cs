2127d6eb83be93c1ff9c264716b63c3e0b5bf764
Red_Phoenix05 <anson.fry@gmail.com>
2025-03-29 07:21:15 -0400

    Update GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -10,11 +11,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject failScreen;
     [SerializeField] GameObject successScreen;
     [SerializeField] List<PickpocketMinigame> tasks = new();
+    [SerializeField] Interactable exitInteractable;
+    [SerializeField] int cash = 0;
+    [SerializeField] TextMeshPro cashDisplay;
 
     private void Awake()
     {
         failScreen.SetActive(false);
         successScreen.SetActive(false);
+        exitInteractable.interactEvent.AddListener(Exit);
+        exitInteractable.enabled = false;
     }
 
     public void GameSuccess()
@@ -37,6 +43,7 @@ public class GameManager : MonoBehaviour
         {
             task.gameObject.SetActive(false);
         }
+        Debug.Log("Game Over: Failure");
     }
 
     public void WarnAll(float value)
@@ -50,16 +57,30 @@ public class GameManager : MonoBehaviour
     public void CheckGameOver()
     {
         Debug.Log("Task completed");
-        bool gameover = true;
+        bool tasksDone = true;
+        cash = 0;
         foreach (PickpocketMinigame task in tasks)
         {
-            gameover &= task.success;
+            tasksDone &= task.success;
+            cash += task.cashValue;
         }
 
+        cashDisplay.text = "$" + cash.ToString();
+
         // end game if all tasks done
-        if (gameover)
+        if (tasksDone)
         {
-            GameSuccess();
+            EnableExit();
         }
+    }
+
+    public void EnableExit()
+    {
+        exitInteractable.enabled = true;
+    }
+
+    public void Exit(bool none)
+    {
+        GameSuccess();
     }
 }
